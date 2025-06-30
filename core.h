33d92b9b885e80d36a31a4395fa81043827e4d30
Amr_Mostafa <amr_21@aucegypt.edu>
2025-06-30 23:30:07 +0300

    abstract base_user class is implemented in core.h

--- core.h
+++ core.h
@@ -1,10 +1,24 @@
 #ifndef CORE_H
 #define CORE_H
 
-class Core
-{
-public:
-    Core();
+#include <QString>
+
+class user {
+    private:
+    QString username;
+    QString password;
+    QString role;
+
+    public:
+    User(const QString& uname, const QString& pword, const QString& r);
+    virtual ~User();
+    QString getUsername() const;
+    QString getRole() const;
+    void setUsername (const QString& newUsername); // for renaming the username after being intialized (i guess)
+
+    virtual bool canManageUsers() const = 0;
+    virtual bool canAddItems() const = 0;
+    virtual bool canGenerateReports() const = 0;
 };
 
 #endif // CORE_H
