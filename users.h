b71779e1756b99c8126a7395c7c56c9bfcc1fb44
Malak <malakelzwawy@aucegypt.edu>
2025-07-01 21:18:44 +0300

    added the derived classes that inherits from the base class user and their functions integrating QT

--- users.h
+++ users.h
@@ -1,10 +1,44 @@
 #ifndef USERS_H
 #define USERS_H
+#include <QString>
+#include <QList>
+#include "User.h"
 
-class Users
-{
+class Admin : public User {
 public:
-    Users();
+    Admin(const QString& uname, const QString& pwd);
+
+
+    bool addUser(QList<User*>& users, const QString& uname, const QString& pwd, const QString& role);
+    bool validatePassword(const QString& pwd) const;
+    void saveUsersToFile(const QList<User*>& users, const QString& filename) const;
+    void loadUsersFromFile(QList<User*>& users, const QString& filename) const;
+    void listUsers(const QList<User*>& users) const;
+    bool deleteUser(QList<User*>& users, const QString& uname);
 };
+
+
+
+class Staff : public User {
+public:
+    Staff(const QString& uname, const QString& pwd);
+
+
+    void viewInventory(const QList<Item>& inventory) const;
+};
+class Manager : public User {
+public:
+    Manager(const QString& uname, const QString& pwd);
+
+
+    void viewInventory(const QList<Item>& inventory) const;
+    bool addItem(QList<Item>& inventory, const Item& newItem);
+
+
+    void generateReport(const QList<Item>& inventory, const QString& filename) const;
+};
+
+
+
 
 #endif // USERS_H
