2a75e3f702acf6c7e7148fd2c92fbe144ea82485
YUSIFABOOD <yousefabood84@gmail.com>
2025-07-10 17:55:00 +0300

    Login ui

---
+++ loginpage.h
@@ -0,0 +1,25 @@
+#ifndef LOGINPAGE_H
+#define LOGINPAGE_H
+
+#include <QWidget>
+
+namespace Ui {
+class LoginPage;
+}
+
+class LoginPage : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit LoginPage(QWidget *parent = nullptr);
+    ~LoginPage();
+
+private slots:
+    void on_pushButton_clicked();
+
+private:
+    Ui::LoginPage *ui;
+};
+
+#endif // LOGINPAGE_H
