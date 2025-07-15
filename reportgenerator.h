cb3346fbb4d97356c06938c710efc6e54be51153
YUSIFABOOD <yousefabood84@gmail.com>
2025-07-15 10:30:27 +0300

    minor fixes

--- reportgenerator.h
+++ reportgenerator.h
@@ -12,13 +12,7 @@ class reportGenerator;
 class reportGenerator : public QDialog
 {
     Q_OBJECT
-protected:
-    void closeEvent(QCloseEvent *event) override {
-        qDebug() << "X button clicked - saving users";
-        UserManager::saveUsers();
-        event->accept();
-        QApplication::quit();  // This makes aboutToQuit work!
-    }
+
 public:
     explicit reportGenerator(QWidget *parent = nullptr);
     ~reportGenerator();
