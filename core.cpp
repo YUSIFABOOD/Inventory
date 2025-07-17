c72e5f4e3abb54ba0d6c44ff2e81307b1b9fd11f
Malak <malakelzwawy@aucegypt.edu>
2025-07-17 10:16:48 +0300

    add item to select the items and reject the items that cannot enter

--- core.cpp
+++ core.cpp
@@ -64,3 +64,23 @@ void UserManager::deleteUser(const QString& name)
 
     usersByUsername.remove(name);
 }
+void InventoryManager::AddItem(QString& name, QString& quantity,   QString& price,   QString& supplier, QString& category  ){
+
+    if(name.isEmpty()||quantity.isEmpty()||price.isEmpty()|| supplier.isEmpty(), category.isEmpty() ){
+        throw runtime_error {"Error: You cannot add an empty item  "};
+
+    }
+    if (quantity.toInt()<0|| price.toInt()<0) {
+        throw runtime_error {"Only postive numbers"};
+    }
+    QString key ;
+    key = name+supplier;
+    if (items.count(key)){
+        throw runtime_error {"Item already exsit"} ;
+
+    }
+ else {
+    items[key]=Item( name, category, quantity.toInt(), price.toInt(), supplier);
+
+    }
+}
